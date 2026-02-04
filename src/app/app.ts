448893313e7a3427b8f29c7ccf2cb4c4c2baaff1
Mayarabnc <mayarabncabral@gmail.com>
2026-02-04 15:02:07 -0300

    Incluindo componentes sobre mim

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,11 @@ import { CabecalhoComponente } from "./cabecalho-componente/cabecalho-componente
 import { ContatoComponente } from "./contato-componente/contato-componente";
 import { IncioComponente } from "./incio-componente/incio-componente";
 import { ProjetosComponente } from "./projetos-componente/projetos-componente";
+import { SobreComponente } from "./sobre-componente/sobre-componente";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CabecalhoComponente, ContatoComponente, IncioComponente, ProjetosComponente],
+  imports: [RouterOutlet, CabecalhoComponente, ContatoComponente, IncioComponente, ProjetosComponente, SobreComponente],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/sobre-componente/sobre-componente.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sobre-componente',
+  imports: [],
+  templateUrl: './sobre-componente.html',
+  styleUrl: './sobre-componente.css',
+})
+export class SobreComponente {
+
+}
