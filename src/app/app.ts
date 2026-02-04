551187c693e947d3d10da05dcac920798985b14a
Mayarabnc <mayarabncabral@gmail.com>
2026-02-04 14:59:40 -0300

    Incluindo componentes projetos

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { CabecalhoComponente } from "./cabecalho-componente/cabecalho-componente";
 import { ContatoComponente } from "./contato-componente/contato-componente";
 import { IncioComponente } from "./incio-componente/incio-componente";
+import { ProjetosComponente } from "./projetos-componente/projetos-componente";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CabecalhoComponente, ContatoComponente, IncioComponente],
+  imports: [RouterOutlet, CabecalhoComponente, ContatoComponente, IncioComponente, ProjetosComponente],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/projetos-componente/projetos-componente.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projetos-componente',
+  imports: [],
+  templateUrl: './projetos-componente.html',
+  styleUrl: './projetos-componente.css',
+})
+export class ProjetosComponente {
+
+}
